585fcf8eb736056c11f09315f8859720f171d87a
Isaac <silong@mtu.edu>
2019-04-04 13:07:07 -0400

    Made changes to Puzzle parser and fixed the errors in the tester

--- Nonogram/src/puzzleParser.java
+++ Nonogram/src/puzzleParser.java
@@ -30,7 +30,7 @@ public class puzzleParser {
 	 * 
 	 */
 
-	public void getClues(List<ArrayList<Integer>> row, List<ArrayList<Integer>> col, List<List<Integer>> puzzle ) {
+	public void getClues(List<ArrayList<Integer>> row, List<ArrayList<Integer>> col, List<ArrayList<Integer>> puzzle ) {
 		int counter = 0;
 		ArrayList<Integer> temp = new ArrayList<>();
 		
