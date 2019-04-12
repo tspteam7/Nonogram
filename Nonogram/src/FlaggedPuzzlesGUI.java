2c93630eafbbafdad2fa914c37442c24a3091dc2
Isaac <silong@mtu.edu>
2019-04-12 11:49:43 -0400

    added gui for flagged puzzles

---
+++ Nonogram/src/FlaggedPuzzlesGUI.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.sql.*;
+public class FlaggedPuzzlesGUI extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String [] args) {
+		launch(args);
+	}
+}
